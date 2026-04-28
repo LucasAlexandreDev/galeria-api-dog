a6aa43183334e78c8d44a2402d3bbce3e23e7032
LUCAS ALEXANDRE DA SILVA <25203702@CFP127ALUNO.LOCAL>
2026-04-28 15:55:24 -0300

    feat: finaliza projeto de galeria dog (consumindo API)

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const getUrlFotos = async function(raca){
+    
+    const base_url  = `https://dog.ceo/api/breed/${raca}/images`
+    const response  = await fetch(base_url)
+    const data      = await response.json()
+
+    return data.message
+}
+
+const criarFoto = function(urlFoto){
+
+    const foto     = document.createElement('img')
+    foto.src       = urlFoto
+    foto.className = 'foto'
+
+    return foto
+}
+
+const preencherGaleria = async function(){
+
+    const galeria  = document.getElementById('container-galeria')
+    const raca     = document.getElementById('raca').value
+    const urlFotos = await getUrlFotos(raca)
+
+    const fotos = urlFotos.map(criarFoto)
+
+    console.log()
+    galeria.replaceChildren(...fotos)
+}
+
+
+
+document.getElementById('pesquisar').addEventListener('click', preencherGaleria)
